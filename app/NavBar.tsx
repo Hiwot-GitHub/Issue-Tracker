0b10ca7a20837e1b6af5ce4bf0d7ca7dc4a9b6db
Sisay Hiwot <angelhiwot@gmail.com>
2024-03-04 12:39:25 +0200

    changing the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { FaBug } from "react-icons/fa";
 import { usePathname } from 'next/navigation';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react'
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 
 const NavBar = () => {
@@ -16,22 +16,30 @@ const NavBar = () => {
         {label: 'Issues', href: '/issues'}
     ]
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/" ><FaBug /></Link>
-    <ul className='flex space-x-6 text-zinc-500'>
-            {links.map(link => <li key={link.href}>
-                <Link href={link.href} 
-                className={classnames({
+    <nav className='border-b mb-5 px-5 py-3'>
+        <Container>
+        <Flex justify='between'>
+          <Flex align='center' gap='3'>
+            <Link href="/" ><FaBug /></Link>
+            <ul className='flex space-x-6 text-zinc-500'>
+                {links.map(link => <li key={link.href}>
+                    <Link href={link.href} 
+                     className={classnames({
                     'text-zinc-900' : link.href === currentPath,
                     'text-zinc-500' : link.href !== currentPath,
                     'hover:text-zinc-800 transition-colors' : true
-                })}>{link.label}</Link></li>)}
+                          })}>{link.label}</Link></li>)}
             
-        </ul>
-        <Box>
-           {status === "authenticated" && <Link href='/api/auth/signout'>Log out</Link> }
-           {status !== "authenticated" && <Link href='/api/auth/signin'>Sign In</Link> }
-        </Box>
+                
+            </ul>
+           </Flex>
+            <Box>
+            {status === "authenticated" && <Link href='/api/auth/signout'>Log out</Link> }
+            {status !== "authenticated" && <Link href='/api/auth/signin'>Sign In</Link> }
+            </Box>
+        </Flex>
+        
+        </Container> 
         
     </nav>
   )
