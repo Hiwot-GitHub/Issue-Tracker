adf76d1c26aa2790556aa2b6010fbf720f35898e
Sisay Hiwot <angelhiwot@gmail.com>
2024-03-08 07:00:10 +0200

    setup react-query

--- app/issues/[id]/Assignee.tsx
+++ app/issues/[id]/Assignee.tsx
@@ -1,17 +1,28 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Select } from '@radix-ui/themes'
+import { User } from '@prisma/client'
+import axios from 'axios'
 
 const Assignee = () => {
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const { data } = await axios.get<User[]>('/api/users');
+            setUsers(data);
+        }
+        fetchUsers();
+    },[])
   return (
     <Select.Root defaultValue="Hiwot">
         <Select.Trigger />
         <Select.Content>
             <Select.Group>
                 <Select.Label>UI Devs</Select.Label>
-                <Select.Item value='Mosh'>Mosh</Select.Item>
-                <Select.Item value='Eric'>Eric</Select.Item>
-                <Select.Item value='Hiwot'>Hiwot</Select.Item>
+                { users.map((user) => (
+                     <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+                ))}
             </Select.Group>
         </Select.Content>
 
